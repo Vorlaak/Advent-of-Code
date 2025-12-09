26c7a650838e4a5c88b9b45382447158eebd0931
Vorlaak <113512926+Vorlaak@users.noreply.github.com>
2025-12-09 10:25:41 +0100

    Completed 2025 Day 2 part 1

---
+++ 2025/02-12/02_12(1).py
@@ -0,0 +1,42 @@
+def get_input():
+    # return "example.txt"
+    return "input.txt"
+
+
+def get_lists(text, range_list):
+    with open(text) as file:
+        ranges = file.read().split(",")
+        for range in ranges:
+            range_list.append((int(range.split("-")[0]), int(range.split("-")[1])))
+
+
+def is_repeating(num):
+    word = str(num)
+    length = len(word)
+    for i in range(1, length // 2 + 2):
+        new_word = word[:i] * 2
+        if new_word == word:
+            return True
+    return False
+
+
+def process_range(start, finish):
+    counter = 0
+    for i in range(start, finish + 1):
+        if len(str(i)) % 2 == 0 and is_repeating(i):
+            counter += i
+    return counter
+
+
+def main():
+    textfile = get_input()
+    range_list = []
+    get_lists(textfile, range_list=range_list)
+    counter = 0
+    for range in range_list:
+        counter += process_range(range[0], range[1])
+    print(counter)
+
+
+if __name__ == "__main__":
+    main()
